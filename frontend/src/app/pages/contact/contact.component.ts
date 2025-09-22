a2ac1d9e08b0e18b51feb7f122ff9de91fb6337b
Dj Redflame <73613085+djredflame@users.noreply.github.com>
2025-09-22 23:29:16 +0200

    fix: fix ci.yml and prettier fixes

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -6,8 +6,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
