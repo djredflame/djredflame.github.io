0706cb3bfb75245f47b69a2866305eeb3558f79d
Dj Redflame <73613085+djredflame@users.noreply.github.com>
2026-06-05 13:02:58 +0200

    fix: update cli to node 24 and remove router-outlet

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/ui/header/header.component';
 import { FooterComponent } from './shared/ui/footer/footer.component';
 
